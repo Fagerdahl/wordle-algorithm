b87b5649f5b13188ba1110026b33a9500c197c83
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-20 00:27:36 +0100

    chore: initial setup complete and ready to implement TDD

---
+++ feedback.js
@@ -0,0 +1,4 @@
+//Ready to import
+export default function algorithmA (){
+    
+};
---
+++ feedback.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from '@jest/globals';
+
+describe('Provtest', () => {
+    it('bör göra nått', () => {
+      expect(true).toBe(true);
+    });
+  });
