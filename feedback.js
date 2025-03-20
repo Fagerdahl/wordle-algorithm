2ec930dd9149b32f1d2d2c342e0d2f9eec5424d6
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-20 21:55:08 +0100

    feat: first set of tests complete, declared runTest for DRY code

--- feedback.js
+++ feedback.js
@@ -1,4 +1,17 @@
 //Minimal implementation of the function to make the test work
-export default function algorithmA (guess, secret) {
-    return guess.split('').map(letter => ({ letter, result: 'correct' }));
-};
+export default function algorithmA(guess, secret) {
+  if (guess.length !== secret.length) {
+    throw new Error("Guess and secret must be the same length!");
+  }
+
+  if (guess === secret) {
+    return guess.split("").map((letter) => ({ letter, result: "correct" }));
+  }
+
+  //Split the string, iterate through every letter, return object based on result
+  return guess.split("").map((letter) => {
+    return secret.includes(letter)
+      ? { letter, result: "misplaced" }
+      : { letter, result: "incorrect" };
+  });
+}
--- feedback.test.js
+++ feedback.test.js
@@ -1,21 +1,31 @@
-import { describe, expect, it } from '@jest/globals';
-import algorithmA from './feedback.js';
+import { describe, expect, it } from "@jest/globals";
+import algorithmA from "./feedback.js";
 
-describe('algorithmA function', () => {
-    it('returns all "correct" when guessed word is the same as the secret word' , () => {
-     const guess = 'RONJA';
-     const secret = 'RONJA';
-     const result = algorithmA(guess, secret);
+//Declaration of a helper function to avoid repetition in every test case, runTest will provide us different indata
+const runTest = (guess, secret, expected) => {
+  expect(algorithmA(guess, secret)).toEqual(expected);
+};
 
-     const expected = [
-      { letter: 'R', result: 'correct' },
-      { letter: 'O', result: 'correct' },
-      { letter: 'N', result: 'correct' },
-      { letter: 'J', result: 'correct' },
-      { letter: 'A', result: 'correct' }
-    ];
-
-    expect(result).toEqual(expected);
-  
-    });
+//Test to verify all letters being correct
+describe("algorithmA function", () => {
+  it('returns all "correct" when guessed word is the same as the secret word', () => {
+    runTest("RONJA", "RONJA", [
+      { letter: "R", result: "correct" },
+      { letter: "O", result: "correct" },
+      { letter: "N", result: "correct" },
+      { letter: "J", result: "correct" },
+      { letter: "A", result: "correct" },
+    ]);
   });
+
+  //Test to verify all letters being incorrect
+  it('returns all "incorrect" when guessed letters are not even part of secret word', () => {
+    runTest("ABCDE", "FGHIJ", [
+      { letter: "A", result: "incorrect" },
+      { letter: "B", result: "incorrect" },
+      { letter: "C", result: "incorrect" },
+      { letter: "D", result: "incorrect" },
+      { letter: "E", result: "incorrect" },
+    ]);
+  });
+});
