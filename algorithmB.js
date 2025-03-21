920b88aefa2cb582ea73a394cba0c78de7a338e3
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-03-21 14:04:52 +0100

    feat: second set of tests complete

--- algorithmB.js
+++ algorithmB.js
@@ -1,1 +1,35 @@
-//function to select a random word based on the specified criteria
+/*functionality to select a random word based on a specified criteria*/ 
+
+export default function algorithmB(words, requiredLength, allowRepeatedLetters) {
+    //filter based on length, only the words with the required amount of letters are considered
+    let filtered = words.filter(word => word.length === requiredLength);
+
+    //If repeated letters are NOT allowed (false), delete repeated words
+    if (!allowRepeatedLetters) {
+        filtered = filtered.filter(word => {
+            const letters = word.split("");
+            return new Set(letters).size === letters.length;
+    });
+}
+  //If no word matches the requirements throw error
+  if (filtered.length === 0) {
+    throw new Error("No word found that matches the criteria");
+  }
+
+  //Random word returned
+  const randomIndex = Math.floor(Math.random() * filtered.length);
+  return filtered[randomIndex];
+}
+
+
+/*
+Notes to enhance my own learning when doing this:
+* words = an array of strings, every string is a word
+* requiredLength = Number describing the required length (integer)
+* allowRepeatedLetters = A boolean true/false deciding if repeating letters are allowed or not
+* The filter() method of Array instances creates a shallow copy of a portion of a given array, 
+filtered down to just the elements from the given array that pass the test implemented by the provided function.
+* Math.random() generates a random number, multiplied with filtered.length we are provided with a decimal
+between 0 and total amount of elements in filtered. 
+* Math.Floor() rounds the decimal to the closest integer
+*/ 
--- algorithmB.test.js
+++ algorithmB.test.js
@@ -1,3 +1,42 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, it } from "@jest/globals";
+import assert from 'node:assert/strict';
 import algorithmB from "./algorithmB.js";
 
+describe('algorithmB', () => {
+  it('function returns a word with the exact required length', () => {
+    const words = ['apple', 'berry', 'dates'];
+    const result = algorithmB(words, 5, true);
+    assert.equal(result.length, 5);
+  });
+
+  it('function returns a word with the exact required length, when all letters are unique', () => {
+    const words = ['apple', 'mango', 'berry'];
+    //"apple" has a double set of 'p', "berry" has a double set of 'r',
+    //"mango" has unique letters and the exact required amount
+    //NO Repeating letters allowed!
+    const result = algorithmB(words, 5, false);
+    const letters = result.split('');
+    assert.equal(new Set(letters).size, letters.length);
+  });
+
+  //function for error handling- no word with correct length
+  it('should throw an error if no word of the required length exists', () => {
+    const words = ['longer', 'tiny'];
+    //None of the words above meet the required length, which means failure
+    assert.throws(() => {
+      algorithmB(words, 5, true);
+    }, /No word found/);
+  });
+
+  //function for error handling - no word with all uniqe letters
+  it('should throw an error if no word with all unique letters exists', () => {
+    const words = ['apple', 'allee', 'broom'];
+    //All words above have a double set of letters, which means failure
+    assert.throws(() => {
+      algorithmB(words, 5, false);
+    }, /No word found/);
+  });
+});
+
+/*I am using callback functions to throw errors. for example: algorithmB(words, 6, true)
+I am using search strings in the form of regular expressions- to verify that our error message is correct*/
